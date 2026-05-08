62626e392d5c516eb1a28bd45effa7cfbe7b9941
sahin997 <mdsahinmia997@gmail.com>
2026-05-08 17:00:03 +0600

    Add async function to connect bot and fetch data

--- public/app.js
+++ public/app.js
@@ -1,1 +1,29 @@
 
+async function connectBot(){
+
+  const number =
+    document.getElementById("number").value;
+
+  const res = await fetch("/pair",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({number})
+  });
+
+  const data = await res.json();
+
+  if(data.success){
+
+    document.getElementById("result")
+      .innerText =
+      "Pair Code: " + data.code;
+
+  } else {
+
+    document.getElementById("result")
+      .innerText =
+      data.error;
+  }
+}
