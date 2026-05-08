c3e7af67bdf77d35a2ea1946fef67d8b66c8e342
sahin997 <mdsahinmia997@gmail.com>
2026-05-08 17:32:01 +0600

    Update server.js

--- server.js
+++ server.js
@@ -24,17 +24,71 @@ async function connectWhatsApp(number) {
     auth: state
   });
 
-  sock.ev.on("creds.update", saveCreds);
+  sock.ev.on(
+    "creds.update",
+    saveCreds
+  );
 
-  await new Promise(resolve =>
+  await new Promise((resolve) =>
     setTimeout(resolve, 5000)
   );
 
   const code =
-    await sock.requestPairingCode(number);
+    await sock.requestPairingCode(
+      number
+    );
 
   return code;
 }
+
+app.post(
+  "/pair",
+  async (req, res) => {
+
+    try {
+
+      let { number } = req.body;
+
+      number =
+        number.replace(
+          /[^0-9]/g,
+          ""
+        );
+
+      const code =
+        await connectWhatsApp(
+          number
+        );
+
+      res.json({
+        success: true,
+        code
+      });
+
+    } catch (e) {
+
+      console.log(e);
+
+      res.json({
+        success: false,
+        error: "Connection Closed"
+      });
+
+    }
+
+  }
+);
+
+const PORT =
+  process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server Running On ${PORT}`
+  );
+
+});}
 
 app.post("/pair", async (req, res) => {
 
